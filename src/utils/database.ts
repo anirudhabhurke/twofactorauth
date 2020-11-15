684d3a6375c77f7121ff54ade465e3bed957d45e
anirudhabhurke <anirudhbhurke@outlook.com>
2020-11-15 10:40:01 +0530

    fix

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -6,10 +6,8 @@ let db: JsonDB | null;
 export const getDb = () => {
       if (!db) {
             db = new JsonDB(new Config('myDatabase', true, true, '/'));
-            console.log('I am a new db');
             return db;
       } else {
-            console.log('I am the same db');
             return db;
       }
 };
